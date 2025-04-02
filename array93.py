7613b0aa13b8041c0587b28227b2558befbca8e8
panneer <panneer.r@refactor.academy>
2025-04-02 17:56:24 +0530

    array93

---
+++ array93.py
@@ -0,0 +1,18 @@
+def isalternating(val,n):
+    if not n > 1:
+        return
+    
+    # find the initial direction.
+    increasing = val[0] < val[1]
+    
+    for i in range(1, n-1):
+        if increasing and val[i] < val[i+1]: # if teh current order needs to check for increase.
+            return 'no'
+        if not increasing and val[i] > val[i+1]: #if the current order is decreasing
+            return 'no'
+        increasing = not increasing
+    return 'yes'
+
+n = int(input())
+a = list(map(int,input().split()))
+print(isalternating(a,n))
